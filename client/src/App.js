390f645a19e093585c5c0c351bd6eb9e75d9d682
Han0612 <allenwzh@outlook.com>
2023-08-18 15:18:00 -0400

    update ...

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom"
 import HomePage from "scenes/homePage"
-import LoginPage from "scenes/loginPage"
+import LoginPage from "scenes/loginPage/Form.jsx"
 import ProfilePage from "scenes/profilePage"
 import { useMemo } from "react"
 import { useSelector } from "react-redux"
 import { CssBaseline, ThemeProvider } from "@mui/material"
 import { createTheme } from "@mui/material/styles"
 import { themeSettings } from "./theme.js"
-import state from "state/index.js"
 
 
 /**
@@ -15,25 +14,23 @@ import state from "state/index.js"
  */
 
 
-
 function App() {
   const mode = useSelector((state) => state.mode)
   // Ensure recalculation only when dependencies change
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+  const isAuth = Boolean(useSelector((state) => state.token))
 
   return (
     <div className="App">
       <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CssBaseline>
-        <Routes>
-          <Route path="/" element={<LoginPage/>} />
-          <Route path="/home" element={<HomePage/>} />
-          <Route path="/profile/:userId" element={<ProfilePage/>} />
-        </Routes>
-        </CssBaseline>
-      </ThemeProvider>
-
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage/>} />
+            <Route path="/home" element={isAuth ? <HomePage/> : <Navigate to="/"/>} />
+            <Route path="/profile/:userId" element={isAuth ? <ProfilePage/> : <Navigate to="/"/>} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   )
